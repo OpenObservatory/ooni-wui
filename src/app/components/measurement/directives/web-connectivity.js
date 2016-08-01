88f1cb381f1c9ba71d3e20aabd0693357c8858df
Arturo Filastò <arturo@filasto.net>
2016-08-01 18:23:34 +0200

    Improvements to the web-connectivity test details page

--- src/app/components/measurement/directives/web-connectivity.js
+++ src/app/components/measurement/directives/web-connectivity.js
@@ -1,13 +1,18 @@
 var angular = require('angular');
 var template = require('./web-connectivity.html');
+var description = require('./web-connectivity.md');
 require('./web-connectivity.scss');
 
 var webConnectivityDirective = angular.module('webConnectivityDirective', [])
-.directive('webConnectivity', function(){
-  return {
-    template: template
+.directive('webConnectivity', ['$sce', function($sce){
+  function link(scope, element, attrs) {
+    scope.description = $sce.trustAsHtml(description);
   }
-})
+  return {
+    template: template,
+    link: link
+  }
+}])
 .name;
 
 module.exports = webConnectivityDirective;
