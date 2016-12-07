4bee1268a06d49bc7e66fa0e0ffb261fcac45bd9
Arturo Filastò <arturo@filasto.net>
2016-12-07 11:51:13 +0000

    Add link to measurements in the explorer

--- src/app/components/measurement/measurement.controller.js
+++ src/app/components/measurement/measurement.controller.js
@@ -1,12 +1,19 @@
 MeasurementController.$inject = ['$stateParams', '$scope', '$http', '$window', 'Measurement'];
 function MeasurementController($stateParams, $scope, $http,  $window, Measurement) {
   $scope.debug = false;
+  var explorerBaseUrl = 'https://explorer.ooni.torproject.org/measurement/';
 
   Measurement.get($stateParams.measurementId, $stateParams.idx)
   .then(function(measurement){
+    var explorerLink;
     $scope.measurement = measurement;
     $scope.testNameLong = measurement.test_name.replace(/_/g, " ");
     $scope.testNameLong = $scope.testNameLong[0].toUpperCase() + $scope.testNameLong.slice(1);
+    explorerLink = explorerBaseUrl + $scope.measurement.report_id;
+    if ($scope.measurement.input) {
+      explorerLink += "?input=" + encodeURI($scope.measurement.input);
+    }
+    $scope.explorerLink = explorerLink;
   }, function(error) {
     console.log("Failed");
     console.log(error);
@@ -15,5 +22,7 @@ function MeasurementController($stateParams, $scope, $http,  $window, Measuremen
   $scope.toggleDebug = function() {
     $scope.debug = !$scope.debug;
   }
+
+
 }
 module.exports = MeasurementController;
