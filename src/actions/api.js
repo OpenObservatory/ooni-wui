596ac2f7aa1d82e54acfad03fa00bda31538c617
Arturo Filastò <arturo@filasto.net>
2016-12-27 22:37:37 +0000

    Add XSRF protection support

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,6 +1,15 @@
-import realFetch from 'isomorphic-fetch';
+import realFetch from 'isomorphic-fetch'
+import cookie from 'react-cookie'
 
-export function fetch(url, options) {
+export function fetch(url, options = {}) {
   // Add XSRF protection in here
+  const xsrfToken = cookie.load('XSRF-TOKEN');
+  if (xsrfToken) {
+    options = {
+      ...options,
+      credentials: 'same-origin',
+      headers: {...options.headers, 'X-XSRF-TOKEN': xsrfToken}
+    }
+  }
   return realFetch(url, options)
 }
