954f152714e08aa17f76dd39074df362df2f5703
Arturo Filastò <arturo@filasto.net>
2017-01-23 17:42:26 +0000

    Point to correct links in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,13 +6,13 @@ export const Footer = () => (
     <div className='container'>
       <ul className='footer-links'>
         <li>
-          <a href='https://ooni.torproject.org/XXX'>About OONI</a>
+          <a href='https://ooni.torproject.org/about/'>About OONI</a>
         </li>
         <li>
-          <a href='https://ooni.torproject.org/XXX'>Legal</a>
+          <a href='https://ooni.torproject.org/about/data-policy/'>Legal</a>
         </li>
         <li>
-          <a href='https://ooni.torproject.org/XXX'>Help</a>
+          <a href='mailto:contact@openobservatory.org'>Help</a>
         </li>
         <li>
           <a href='https://explorer.ooni.torproject.org/'>Explore data</a>
