a6594bcc975973d793e1a1a38272ce7bcb27f89c
Simone Basso <bassosimone@gmail.com>
2017-02-15 23:01:57 +0100

    Make TcpConnect.js ready for i18n

--- src/routes/Measurements/components/nettests/TcpConnect.js
+++ src/routes/Measurements/components/nettests/TcpConnect.js
@@ -5,15 +5,32 @@ export const TcpConnectDetails = ({ measurement }) => {
     <div>
       {measurement.test_keys.connection === 'success' &&
       <div>
-        <h2 className='result-success'><i className='fa fa-check-circle-o' /> Everything is OK</h2>
+        <h2 className='result-success'><i className='fa fa-check-circle-o' />
+          <FormattedMessage
+            id='nettests.tcpConnect.EverythingOkay'
+            defaultValue='Everything is OK'
+          />
+        </h2>
       </div>
       }
 
       {measurement.test_keys.connection !== 'success' &&
       <div>
-        <h2 className='result-warning'><i className='fa fa-times-circle-o' /> Connection problem</h2>
-        <p>When connecting to <code>{measurement.input}</code> we got the error
-          <code>{measurement.test_keys.connection}</code>
+        <h2 className='result-warning'><i className='fa fa-times-circle-o' />
+          <FormattedMessage
+            id='nettests.tcpConnect.ConnectionProblem'
+            defaultValue='Connection problem'
+          />
+        </h2>
+        <p>
+          <FormattedMessage
+           id='nettests.tcpConnect.Explanation'
+           defaultValue='When connecting to {input} we got the error {error}'
+           values={{
+             input: <code>{measurement.input}</code>,
+             error: <code>{measurement.test_keys.connection}</code>
+           }}
+          />
         </p>
       </div>
       }
