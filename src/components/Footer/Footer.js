854821b37e1797e462d6bf0dca7debdac5aa50f9
Simone Basso <bassosimone@gmail.com>
2017-02-15 23:21:38 +0100

    Footer.js: UPGRADED! UPGRADED! UPGRADED!

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,37 @@ export const Footer = () => (
     <div className='container'>
       <ul className='footer-links'>
         <li>
-          <a href='https://ooni.torproject.org/about/' target='_blank'>About OONI</a>
+          <a href='https://ooni.torproject.org/about/' target='_blank'>
+            <FormattedMessage
+             id='footer.AboutOoni'
+             defaultValue='About OONI'
+            />
+          </a>
         </li>
         <li>
-          <a href='https://ooni.torproject.org/about/data-policy/' target='_blank'>Data policy</a>
+          <a href='https://ooni.torproject.org/about/data-policy/'
+             target='_blank'>
+               <FormattedMessage
+                 id='footer.DataPolicy'
+                 defaultValue='Data policy'
+               />
+          </a>
         </li>
         <li>
-          <a href='mailto:contact@openobservatory.org' target='_blank'>Help</a>
+          <a href='mailto:contact@openobservatory.org' target='_blank'>
+            <FormattedMessage
+              id='footer.Help'
+              defaultValue='Help'
+            />
+          </a>
         </li>
         <li>
-          <a href='https://explorer.ooni.torproject.org/' target='_blank'>Explore data</a>
+          <a href='https://explorer.ooni.torproject.org/' target='_blank'>
+            <FormattedMessage
+              id='footer.ExploreData'
+              defaultValue='Explore data'
+            />
+          </a>
         </li>
       </ul>
     </div>
