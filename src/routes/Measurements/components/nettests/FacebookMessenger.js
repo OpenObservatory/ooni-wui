fffb100c970f06f47029528af6cd9f4e530f9e16
Simone Basso <bassosimone@gmail.com>
2017-02-15 23:16:04 +0100

    i18nify FacebookMessenger.js

--- src/routes/Measurements/components/nettests/FacebookMessenger.js
+++ src/routes/Measurements/components/nettests/FacebookMessenger.js
@@ -6,21 +6,64 @@ export const FacebookMessengerDetails = ({ measurement }) => {
       {measurement.test_keys.facebook_dns_blocking === false &&
        measurement.test_keys.facebook_tcp_blocking === false &&
        <div>
-         <h2 className='result-success'><i className='fa fa-check-circle-o' /> Facebook Messenger is working</h2>
+         <h2 className='result-success'><i className='fa fa-check-circle-o' />
+           <FormattedMessaged
+             id='nettests.facebookMessenger.IsWorking'
+             defaultValue='Facebook Messenger is working'
+           />
+          </h2>
        </div>
       }
       {measurement.test_keys.facebook_dns_blocking === true &&
       <div>
-        <h2 className='result-danger'><i className='fa fa-times-circle-o' /> Evidence of censorship</h2>
-        <p>Facebook Messenger appears to be <strong className='text-danger'>blocked</strong> via <strong>DNS</strong>
+        <h2 className='result-danger'><i className='fa fa-times-circle-o' />
+          <FormattedMessage
+            id='nettests.facebookMessenger.DnsBlocking.evidenceOfCensorship'
+            defaultValue='Evidence of censorship'
+          />
+        </h2>
+        <p>
+          <FormattedMessage
+            id='nettests.facebookMessenger.DnsBlocking.details'
+            defaultValue='Facebook Messenger appears to be {blocked} via {reason}'
+            values={{
+              blocked:
+                <strong className='text-danger'>
+                  <FormattedMessage
+                    id='nettests.facebookMessenger.DnsBlocking.details.blocked'
+                    defaultValue='blocked'
+                  />
+                </strong>,
+              reason: <strong>DNS</strong>
+            }}
+          />
         </p>
       </div>
       }
 
       {measurement.test_keys.facebook_tcp_blocking === true &&
       <div>
-        <h2 className='result-danger'><i className='fa fa-times-circle-o' /> Evidence of censorship</h2>
-        <p>Facebook Messenger appears to be <strong className='text-danger'>blocked</strong> via <strong>TCP/IP</strong>
+        <h2 className='result-danger'><i className='fa fa-times-circle-o' />
+          <FormattedMessage
+            id='nettests.facebookMessenger.TcpIpBlocking.evidenceOfCensorship'
+            defaultValue='Evidence of censorship'
+          />
+        </h2>
+        <p>
+          <FormattedMessage
+            id='nettests.facebookMessenger.TcpIpBlocking.details'
+            defaultValue='Facebook Messenger appears to be {blocked} via {reason}'
+            values={{
+              blocked:
+                <strong>
+                  <FormattedMessage
+                    id='nettests.facebookMessenger.TcpIpBlocking.details.blocked'
+                    defaultValue='blocked'
+                  />
+                </strong>,
+              reason: <strong>TCP/IP</strong>
+            }}
+          />
         </p>
       </div>
       }
