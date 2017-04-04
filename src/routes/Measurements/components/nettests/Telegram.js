3168f0647a7caf7f40da07b630cbdcee353344fd
Arturo Filastò <arturo@filasto.net>
2017-04-04 11:45:18 +0000

    Add UI elements to show blocking of Telegram Web

--- src/routes/Measurements/components/nettests/Telegram.js
+++ src/routes/Measurements/components/nettests/Telegram.js
@@ -6,6 +6,7 @@ export const TelegramDetails = ({ measurement }) => {
     <div>
       {measurement.test_keys.telegram_tcp_blocking === false &&
        measurement.test_keys.telegram_http_blocking === false &&
+       measurement.test_keys.telegram_web_status === 'ok' &&
        <div>
          <h2 className='result-success'><i className='fa fa-check-circle-o' />
            <FormattedMessage
@@ -16,7 +17,9 @@ export const TelegramDetails = ({ measurement }) => {
        </div>
       }
 
-      {measurement.test_keys.telegram_tcp_blocking === true || measurement.test_keys.telegram_http_blocking === true &&
+      {measurement.test_keys.telegram_tcp_blocking === true ||
+      measurement.test_keys.telegram_web_status === 'blocked' ||
+      measurement.test_keys.telegram_http_blocking === true &&
       <div>
         <h2 className='result-success'><i className='fa fa-check-circle-o' />
           <FormattedMessage
@@ -26,6 +29,70 @@ export const TelegramDetails = ({ measurement }) => {
         </h2>
       </div>
       }
+
+      {(measurement.test_keys.telegram_tcp_blocking === true ||
+      measurement.test_keys.telegram_http_blocking === true) &&
+      <p>
+        <FormattedMessage
+          id='nettests.telegram.applicationBlocked'
+          defaultMessage='The Telegram application appears to be {status}.'
+          values={{
+            status: <strong className='text-danger'>
+              <FormattedMessage
+                id='nettests.telegram.applicationBlocked.statusBlocked'
+                defaultMessage='blocked'
+              /></strong>
+          }}
+        />
+      </p>
+      }
+
+      {measurement.test_keys.telegram_tcp_blocking === false &&
+      measurement.test_keys.telegram_http_blocking === false &&
+      <p>
+        <FormattedMessage
+          id='nettests.telegram.notBlocked'
+          defaultMessage='The Telegram application appears to be {status}.'
+          values={{
+            status: <strong className='text-success'>
+              <FormattedMessage
+                id='nettests.telegram.notBlocked.workingProperly'
+                defaultMessage='working properly'
+              /></strong>
+          }}
+      />
+      </p>
+      }
+
+      {measurement.test_keys.telegram_web_status === 'blocked'
+      ? <p>
+        <FormattedMessage
+          id='nettests.telegram.webBlocked'
+          defaultMessage='Telegram web appears to be {status}.'
+          values={{
+            status: <strong className='text-danger'>
+              <FormattedMessage
+                id='nettests.telegram.webBlocked.blocked'
+                defaultMessage='blocked'
+              /></strong>
+          }}
+        />
+      </p>
+      : <p>
+        <FormattedMessage
+          id='nettests.telegram.webWorking'
+          defaultMessage='Telegram web appears to be {status}.'
+          values={{
+            status: <strong className='text-success'>
+              <FormattedMessage
+                id='nettests.telegram.webWorking.properly'
+                defaultMessage='working properly'
+              /></strong>
+          }}
+        />
+      </p>
+      }
+
     </div>
   )
 }
